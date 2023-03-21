585fd74379980bcb63fa287eca68ce1f7a269380
wannafuckingsleep <wannasleeptoday@gmail.com>
2023-03-21 23:00:48 +0600

    add types of Chat and Event

--- utils/types.py
+++ utils/types.py
@@ -1,59 +1,62 @@
-from typing import Union, Optional
+from typing import Optional
+from datetime import datetime
 
 
-class Colors:
-    blue = "blue"
-    green = "green"
-    red = "red"
-    white = "white"
-
-
-def button(
-         text: str = None,
-         visible_text: Union[str, bool] = None,
-         callback: bool = False,
-         color: str = Colors.blue,
-) -> dict:
-
+class Chat:
     """
-    Если нужна новая строка, вызывается без параметров
-    :param text: Задается текст
-    :param visible_text: Видимый текст
-    :param callback: True if callback
-    :param color: use Colors.*red*
-    :return: словарь для генерации в клавиатуру для платформы
+        Объект чата
     """
 
-    # if new_line:
-    #     return {"text": "line"}
-    if not any((text, visible_text)):
-        return {"text": "line"}
-    if visible_text is None:
-        visible_text = text
-
-    current_button = {
-        "text": text,
-        "visible_text": visible_text,
-        "color": color
-    }
-    if callback:
-        current_button['type'] = "callback"
-    return current_button
+    def __init__(
+            self,
+            peer_id: int,
+            thread_id: Optional[int],
+            delete_message: int,
+            start_date: datetime,
+    ):
+        self.peer_id: int = peer_id
+        self.thread_id: Optional[int] = thread_id  # Для использования бота в топиках
+        self.delete_message: int = delete_message  # Поле для информации о автоудалении собщений
+        self.start_date: datetime = start_date  # Дата добавления бота в чат
 
 
-class Keyboard:
-    example = (
-        button(
-            visible_text="Видимый текст",
-            text="Вводимый текст",
-            color=Colors.green,
-            callback=False
-        ),
-        button(text="line"),
-        button(
-            visible_text="Видимый текст",
-            text="Вводимый текст",
-            color=Colors.blue,
-            callback=True
-        ),
-    )
+class Event:
+    """
+        Класс для удобного обращения к объектам ивента от платформ
+    """
+
+    def __init__(
+            self,
+            user_id: int = None,
+            peer_id: int = None,
+            username: Optional[str] = None,
+            message: str = None,
+            param: Optional[str] = None,
+            attachment: Optional[str] = None,
+            reply_from: Optional[str] = None,
+            message_id: Optional[int] = None,
+            callback_id: Optional[int] = None,
+            destination: Optional[int] = None,
+            is_bot: bool = None,
+            chat: Optional[Chat] = None,
+            thread_id: Optional[int] = None,
+            is_forum: Optional[bool] = None,
+    ):
+        self.user_id: int = user_id
+        self.peer_id: int = peer_id
+        self.username: Optional[str] = username
+        self.is_bot: bool = is_bot
+
+        self.message: str = message
+        self.param: Optional[str, None] = param
+        self.attachment: Optional[str] = attachment
+        self.chat: Optional[Chat] = chat
+        self.thread_id: Optional[int] = thread_id
+
+        self.reply_from: Optional[int, None] = reply_from
+        self.destination: Optional[int] = destination
+
+        self.message_id: Optional[int, None] = message_id
+        self.callback_id: Optional[int, None] = callback_id
+
+        self.is_forum: Optional[bool] = is_forum
