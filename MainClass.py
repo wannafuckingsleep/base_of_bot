ba212831af73c5f6ac53bfd49cc9c1a6746a9c7d
wannafuckingsleep <wannasleeptoday@gmail.com>
2023-03-21 22:46:49 +0600

    add imports and lock

--- MainClass.py
+++ MainClass.py
@@ -1,4 +1,18 @@
+import asyncio
+import nest_asyncio
+from utils.types import Keyboard
+
+nest_asyncio.apply()
+
+
 class Main:
+
+    # Лок, который блокирует одновременное выполнение одинаковых команд в одном чате.
+    # Используется в CommandClass.py
+    command_lock: dict = {
+        "queue": {},
+        "message": "Попробуйте использовать команду через 5 секунд"  # Текст, который будет выдавать лок
+    }
 
     def __init__(self):
         pass
